8f5f4724501d5966f5b7611793e0ede4e71e37df
Nakamuramasa <ebrten14@gmail.com>
2020-10-04 16:21:48 +0900

    Handling for login sign up and log in

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+<?php
+include("includes/config.php");
+
+// session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])) $userLoggedIn = $_SESSION['userLoggedIn'];
+else header("Location: register.php");
+?>
+
 <!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>Hello</title>
-    </head>
-    <body>
-        <h1>Hello World from Enginx!!</h1>
-    </body>
+<html lang="en">
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello World from Enginx!!</h1>
+</body>
 </html>
